87aa78051160b83b14cd4774e3ae775ceeed2193
Guus Groenink <Guus@Biggest-cock-in-the-office.local>
2021-01-15 18:35:25 +0100

    update Guus

--- index.md
+++ index.md
@@ -1,4 +1,35 @@
 ---
 title: Datavis
 layout: home
---- 
+articles:
+    -  title: Criminaliteit tijdens de Covid-19 periode
+       markdown: |
+            We leven in een bijzondere tijd. De pandemie heeft ons slecht getroffen, vooral onze gezondheid. Hierdoor hebben wij ons moeten aanpassen. Dit was ook een harde klap voor de criminaliteit, waardoor een aantal misdaden zijn verminderd. Dit is wel goed nieuws zou je zeggen. Maar desondanks heeft dit geleid tot het ontstaan van nieuwe misdaden en een stijging bij bestaande misdaden. Met andere woorden de criminaliteit heeft zich ook aangepast.
+            
+            Zo is de zakkenrollerij gedaald en zijn de aantal inbraken verminderd, maar jammer genoeg is hierdoor een nieuwe deur geopend voor criminelen, namelijk Cybercrime. Opgelicht worden op het internet. Een ervaring die je niemand gunt!
+
+            We nemen je mee in de cijfers en de verhalen van gedupeerden en stellen je graag op de hoogte van wat er precies veranderd is.
+cijfers_articles:
+    - markdown: |
+        ## Uit cijfers van de politie blijkt dat er 
+        ### 590498
+        
+        gedupeerden van criminaliteit waren tijdens de pandemie. Dit is een weergave van de hoeveelheid mensen die getroffen zijn door verschillende misdaden. COVID-19 heeft veel economische en humanitaire verliezen veroorzaakt. Daarnaast heeft het virus een grote impact op de ontwikkeling van criminaliteit in Nederland.
+    - markdown: |
+        ## 34% hiervan was cybercrime
+        ### 345569
+        Gedupeerden tijdens de pandemie volgens de politiecijfers vanaf 1 maart 2020 tot en met 30 november 2020. Dit is een weergave van de hoeveelheid mensen die getroffen zijn door bepaalde misdaden.
+
+        Niet alle criminelen zijn inactief door de pandemie. Het aantal fraudegevallen is sterk toegenomen. Afgelopen jaar werden in maart bijna 9.000 gevallen van fraude gemeld, tegenover 6.600 een jaar geleden. Het gaat daarbij vooral om de kwestie van digitale fraude. 34% hiervan is cybercrime. in braak en % was zakkenrollerij.
+krantenkoppen: 
+    - title: Hoi brand moord corona crisis 
+      url: 
+    - title: Hoi brand moord corona crisis 
+      url: 
+    - title: Hoi brand moord corona crisis 
+      url: 
+    - title: Hoi brand moord corona crisis 
+      url: 
+    - title: Hoi brand moord corona crisis 
+      url: 
+---
--- js/script.js
+++ js/script.js
@@ -1,73 +1,91 @@
-const offsetMar = document.querySelector('#mar').offsetTop,
-offsetApr = document.querySelector('#apr').offsetTop,
-offsetMay = document.querySelector('#may').offsetTop,
-offsetJun = document.querySelector('#jun').offsetTop,
-offsetJul = document.querySelector('#jul').offsetTop,
-offsetAug = document.querySelector('#aug').offsetTop,
-offsetSep = document.querySelector('#sep').offsetTop,
-offsetOkt = document.querySelector('#okt').offsetTop,
-offsetNov = document.querySelector('#nov').offsetTop;
+var sections = [];
+function getAllSections() {
+    var allSections = document.querySelectorAll('main > section');
+    allSections.forEach(
+        function (section) {
+            var d = {
+                name: section.getAttribute('id'),
+                height: section.offsetHeight,
+                offset: section.offsetTop,
+                el: section
+            }
+            sections.push(d);
+        });
+}
+getAllSections();
 
-var currentMonthDom;
 
 function scrollFunction() {
-    let scrollPos = window.scrollY; 
-    setBodyClass(scrollPos);
-    if(scrollPos > offsetMar) {
-        // scroll gets the scrollPos minus where the last offset is (to start at 0 again), then gets the speed
-        // by dividing the height by so 100% will be the end. Then minus 100 because the starting place
-        // of the overflow div is -100%. Then we + 8.5% for every itreration it has gone. 
-        let scroll = parseFloat(((((scrollPos - currentMonthDom.offsetTop) / (currentMonthDom.offsetHeight)) / 12.5 * 100) - 100) + (8.3 * document.body.getAttribute("data-multiplier")));
-        console.log(currentMonthDom.offsetHeight);
-        document.querySelector(".color").style = `transform: translateX(${scroll}%)`
+    let scrollPos = window.scrollY;
+    currentSectionObj = getSection(scrollPos);
+    document.body.className = currentSectionObj.name;
+    innerPos = parseInt(scrollPos - currentSectionObj.offset) / currentSectionObj.height + 0.45;
+
+    console.log(innerPos);
+
+    if (currentSectionObj.name == "introduction") {
+        addClassAt("cloud1", 0.5, innerPos);
+        addClassAt("cloud2", 0.7, innerPos);
+        addClassAt("noclouds", 0.95, innerPos);
+    }
+    
+    if (currentSectionObj.name == "cijfers") {
+        if (!currentSectionObj.el.classList.contains("counterStarted")) startCounter(currentSectionObj.el);
+        addClassAt("showBlocks", 0.5, innerPos);
+        addClassAt("overflow", 0.55, innerPos);
+        addClassAt("block1", 0.65, innerPos);
+        addClassAt("block2", 0.70, innerPos);
+        addClassAt("block3", 0.75, innerPos);
+        addClassAt("inbraken", 0.9, innerPos);
+    }
+}
+
+function startCounter(el) {
+    el.classList.add("counterStarted");
+    var countSpan = el.querySelector('figure span'),
+        counterInterval = setInterval(
+            function () {
+                var counter = parseInt(countSpan.textContent);
+                counter = counter + 421;
+                if (counter > 590498) {
+                    clearInterval(counterInterval);
+                    counter = 590498;
+                    setInterval(function () { countSlowly(el) }, 1000);
+                }
+                countSpan.textContent = counter;
+            }, 1
+        );
+}
+
+function countSlowly(el) {
+    var countSpan = el.querySelector('figure span'),
+        counter = parseInt(countSpan.textContent);
+    counter++;
+    countSpan.textContent = counter;
+}
+
+function addClassAt(name, percentage, innerPos) {
+    var currentSectionEl = getSection(window.scrollY).el;
+    if (innerPos >= percentage) currentSectionEl.classList.add(name);
+    else if (innerPos < percentage) currentSectionEl.classList.remove(name);
+}
+
+function getSection(y) {
+    var i;
+    y = parseInt(y + (window.innerHeight / 4));
+    for (i = 0; i < sections.length; i++) {
+        if (!sections[i + 1]) return sections[sections.length - 1];
+        if (y >= sections[i].offset && y < sections[i + 1].offset) {
+            return sections[i];
+        }
     }
 }
 
 window.addEventListener('scroll', scrollFunction);
 scrollFunction();
 
-
-function setBodyClass(scrollPos) {  
-    var classToAdd= "top",
-        multiplier = 0;
-    if(scrollPos >= offsetMar && scrollPos < offsetApr ) {
-        classToAdd = "mar";
-        multiplier = 0;
-    }
-    else if(scrollPos >= offsetApr && scrollPos < offsetMay ) {
-        classToAdd = "apr";
-        multiplier = 1;
-    }
-    else if(scrollPos >= offsetMay && scrollPos < offsetJun ) {
-        classToAdd = "may";
-        multiplier = 2;
-    }
-    else if(scrollPos >= offsetJun && scrollPos < offsetJul ) {
-        classToAdd = "jun";
-        multiplier = 3;
-    }
-    else if(scrollPos >= offsetJul && scrollPos < offsetAug ) {
-        classToAdd = "jul";
-        multiplier = 4;
-    }
-    else if(scrollPos >= offsetAug && scrollPos < offsetSep ) {
-        classToAdd = "aug";
-        multiplier = 5;
-    }
-    else if(scrollPos >= offsetSep && scrollPos < offsetOkt ) {
-        classToAdd = "sep";
-        multiplier = 6;
-    }
-    else if(scrollPos >= offsetOkt && scrollPos < offsetNov ) {
-        classToAdd = "okt";
-        multiplier = 7;
-    }
-    else if(scrollPos >= offsetNov) { 
-        classToAdd = "nov";
-        multiplier = 8;
-    }
-
-    currentMonthDom = document.querySelector("section#" + classToAdd);
-    document.body.setAttribute('data-multiplier', multiplier);
-    document.body.className = (classToAdd);
-}
+// scroll gets the scrollPos minus where the last offset is (to start at 0 again), then gets the speed
+// by dividing the height by so 100% will be the end. Then minus 100 because the starting place
+// of the overflow div is -100%. Then we + 8.5% for every itreration it has gone. 
+// let scroll = parseFloat(((((scrollPos - currentMonthDom.offsetTop) / (currentMonthDom.offsetHeight)) / 12.5 * 100) - 100) + (8.3 * document.body.getAttribute("data-multiplier")));
+// console.log(currentMonthDom.offsetHeight);
